d4b631e29c805ffb251533d58081c37ebc07f7f7
finnhodgkin <finnhodgkin@gmail.com>
2017-03-03 12:44:05 +0000

    clean up addTimeToDom function relates to #30

--- tests/tests.js
+++ tests/tests.js
@@ -177,7 +177,7 @@ test('manipulate the dom to show without stop time', (assert) => {
 module('createLapElement');
 test('createLapElement makes a human readable lap element', (assert) => {
   var result = createLapElement(0, 500, 'testing').innerText;
-  var expected = document.getElementById('laptest').innerText;
+  var expected = 'Lap number 1 00:00.50';
   assert.equal(result, expected);
 });
 
